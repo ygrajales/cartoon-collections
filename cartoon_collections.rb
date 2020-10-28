e48632a2140bf16b819d715a61c73093e2d7b329
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-10-28 16:05:37 -0500

    commit tree 972ac3994ebf0f67e75a22ba7d3183a731b16209

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(roll)
+  roll.each.with_index(1){|name, i| puts "#{i}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(team)
+  elements = team.map{|planeteer| planeteer.capitalize}
+  elements.collect{|element| element + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call_list)
+  call_list.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  ingredients.find{|item| cheese_types.include?(item)}
+  
 end
